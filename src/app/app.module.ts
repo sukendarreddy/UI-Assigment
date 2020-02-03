b459bfac4854890d476a82fa16ff41b31ab79864
sukendar <sukendarreddy.98@gmail.com>
2020-02-03 12:16:06 +0530

    app changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Imports for loading & configuring the in-memory web api
+import { HttpClientInMemoryWebApiModule  } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {  NotesData } from './notes-data';
+import { HomeComponent } from './home/home.component';
+import { AddEditNotesComponent } from './add-edit-notes/add-edit-notes.component';
+import { NotesEditGuard } from './services/add-edit-gruard.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddEditNotesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientInMemoryWebApiModule.forRoot(NotesData),
   ],
-  providers: [],
+  providers: [NotesEditGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
